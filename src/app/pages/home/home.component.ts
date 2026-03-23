f71431de3dc4c09736a19822b9db7ed1b81d427c
GABBRIELJUA <20221040@uthh.edu.mx>
2026-03-23 10:48:32 -0600

    agregado gráfico de distribución por departamento en el componente de inicio

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from "@angular/router";
+import { AfterViewInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 
 import { DashboardService } from '../../services/dashboard.service';
@@ -19,7 +21,7 @@ export interface DashboardSummary {
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit, OnInit {
 
   private dashboardService = inject(DashboardService);
   // Datos de ejemplo que luego vendrán de un Servicio/API
@@ -46,6 +48,12 @@ export class HomeComponent implements OnInit {
       next: (res) => {
         this.distribucionDepartamentos = res;
         this.loading = false;
+
+        // 👇 IMPORTANTE
+        setTimeout(() => {
+          this.renderChart();
+        }, 0);
+
       },
       error: () => {
         this.loading = false;
@@ -53,8 +61,59 @@ export class HomeComponent implements OnInit {
       }
     });
   }
-trackByDepartamento(index: number, item: any): string {
-  return item.departamento;
-}
+  trackByDepartamento(index: number, item: any): string {
+    return item.departamento;
+  }
+
+
+  chart: any;
+
+  colors = [
+    '#f97316', // naranja
+    '#3b82f6', // azul
+    '#10b981', // verde
+    '#a855f7', // morado
+    '#ec4899', // rosa
+    '#14b8a6',
+    '#f59e0b'
+  ];
+
+  ngAfterViewInit() {
+    this.renderChart();
+  }
+
+  renderChart() {
+
+    if (this.chart) {
+      this.chart.destroy(); // 💣 evita duplicados
+    }
+
+    const labels = this.distribucionDepartamentos.map(d => d.departamento);
+    const data = this.distribucionDepartamentos.map(d => d.total);
+
+    const ctx = document.getElementById('chartDepartamentos') as any;
+
+    if (!ctx) return;
+
+    this.chart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels,
+        datasets: [{
+          data,
+          backgroundColor: this.colors,
+          borderWidth: 0
+        }]
+      },
+      options: {
+        cutout: '70%',
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      }
+    });
+  }
 
 }
